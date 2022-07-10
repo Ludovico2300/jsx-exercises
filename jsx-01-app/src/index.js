74304de5a89cae7aa9787f86c9f7309387f546f9
Ludovico Colucci <ludovicocolucci@gmail.com>
2022-07-10 16:11:50 +0200

    add jsx-01

---
+++ jsx-01-app/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const hello = "Hello World";
+
+
+
+root.render(
+    <React.StrictMode>
+      <h1>{hello}</h1>
+    </React.StrictMode>
+  );
+
